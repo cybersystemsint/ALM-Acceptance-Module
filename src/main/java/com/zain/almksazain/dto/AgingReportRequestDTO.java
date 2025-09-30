69e59bbedbd4be27d83808a5658753c1bac59ecb
Essyuge <ugeesther@gmail.com>
2025-09-30 04:50:36 +0300

    add feat:aging report and aging report dashboard

--- src/main/java/com/zain/almksazain/dto/AgingReportRequestDTO.java
+++ src/main/java/com/zain/almksazain/dto/AgingReportRequestDTO.java
@@ -1,37 +1,50 @@
 package com.zain.almksazain.dto;
-
 public class AgingReportRequestDTO {
-    
+    private String supplierId;
     private String columnName;
     private String searchQuery;
-    private int page;
-    private int size;
-    // getters and setters
+    private Integer page;
+    private Integer size;
+
+    // Getters and Setters
+
+    public String getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(String supplierId) {
+        this.supplierId = supplierId;
+    }
+
     public String getColumnName() {
         return columnName;
     }
+
     public void setColumnName(String columnName) {
         this.columnName = columnName;
     }
+
     public String getSearchQuery() {
         return searchQuery;
     }
+
     public void setSearchQuery(String searchQuery) {
         this.searchQuery = searchQuery;
     }
-    public int getPage() {
+
+    public Integer getPage() {
         return page;
     }
-    public void setPage(int page) {
+
+    public void setPage(Integer page) {
         this.page = page;
     }
-    public int getSize() {
+
+    public Integer getSize() {
         return size;
     }
-    public void setSize(int size) {
+
+    public void setSize(Integer size) {
         this.size = size;
     }
-
-
-    
 }
